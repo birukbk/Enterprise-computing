8e9c75688999418a27f1f4b3195a846434e7f112
Biruk G <birukbk@gmail.com>
2017-03-06 17:58:34 +0000

    Update app_api GET book reviews.

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,10 +1,59 @@
 var mongoose = require('mongoose');
 var Loc = mongoose.model('Book');
-var sendJsonResponse = function(res,status,content){
+var sendJSONresponse = function(res,status,content){
 	res.status(status);
 	res.json(content);
 };
 
-module.exports.reviewsReadOne = function(req,res){
-	sendJsonResponse(res,200,{"status":"success"});
-	};
+// module.exports.reviewsReadOne = function(req,res){
+// 	sendJsonResponse(res,200,{"status":"success"});
+// 	};
+// 	
+module.exports.reviewsReadOne = function(req, res) {
+  console.log("Getting single review");
+  if (req.params && req.params.bookid && req.params.reviewid) {
+    Loc
+      .findById(req.params.bookid)
+      .select('name reviews')
+      .exec(
+        function(err, book) {
+          console.log(book);
+          var response, review;
+          if (!book) {
+            sendJSONresponse(res, 404, {
+              "message": "bookid not found"
+            });
+            return;
+          } else if (err) {
+            sendJSONresponse(res, 400, err);
+            return;
+          }
+          if (book.reviews && book.reviews.length > 0) {
+            review = book.reviews.id(req.params.reviewid);
+            if (!review) {
+              sendJSONresponse(res, 404, {
+                "message": "reviewid not found"
+              });
+            } else {
+              response = {
+                book: {
+                  name: book.name,
+                  id: req.params.bookid
+                },
+                review: review
+              };
+              sendJSONresponse(res, 200, response);
+            }
+          } else {
+            sendJSONresponse(res, 404, {
+              "message": "No reviews found"
+            });
+          }
+        }
+    );
+  } else {
+    sendJSONresponse(res, 404, {
+      "message": "Not found, bookid and reviewid are both required"
+    });
+  }
+};
