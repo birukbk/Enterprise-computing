4116f24a50b3830703ccee63e1fd04cec2a2a16d
Biruk G <birukbk@gmail.com>
2017-03-20 21:42:55 +0000

    Minor edits. removes error messages.

--- app_server/controllers/books.js
+++ app_server/controllers/books.js
@@ -18,25 +18,6 @@ var renderHomepage = function(req, res, responseBody) {
     });
 };
 
-
-var _showError = function (req, res, status) {
-  var title, content;
-  if (status === 404) {
-    title = "404, page not found";
-    content = "Oh dear. Looks like we can't find this page. Sorry.";
-  } else if (status === 500) {
-    title = "500, internal server error";
-    content = "How embarrassing. There's a problem with our server.";
-  } else {
-    title = status + ", something's gone wrong";
-    content = "Something, somewhere, has gone just a little bit wrong.";
-  }
-  res.status(status);
-  res.render('generic-text', {
-    title : title,
-    content : content
-  });
-};
 /* GET 'home' page */
 module.exports.homelist = function(req, res){
   var requestOptions, path;
@@ -76,9 +57,6 @@ var getbookInfo = function (req, res, callback) {
     }
   );
 };
-
-
-
 /* GET Book info page */
 module.exports.bookInfo = function(req, res){
   res.render('book-info', { title: 'Book Info' });
