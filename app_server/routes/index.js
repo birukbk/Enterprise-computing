b99978636eafb429bd2271084cabdf1d74a2d672
Biruk G <birukbk@gmail.com>
2017-03-21 19:28:15 +0000

    Adds router for bookInfo, details view.

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,7 +5,7 @@ var ctrlOthers = require('../controllers/others');
 
 /* book pages */
 router.get('/', ctrlBook.homelist);
-//router.get('/book/:bookid', ctrlBook.bookInfo);
+router.get('/book/:bookid', ctrlBook.bookInfo);
 
 router.get('/book/:bookid/review/new', ctrlBook.addReview);
 router.post('/book/:bookid/review/new', ctrlBook.doAddReview);
