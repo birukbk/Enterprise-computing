7efc5b20c9ae0cf2f35d0b35ede9ef630a8dfeb1
Biruk G <birukbk@gmail.com>
2017-03-23 15:52:36 +0000

    adds Authentication middleware to specific routes.

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+
+var jwt = require('express-jwt');
+var auth = jwt({
+  secret: process.env.JWT_SECRET,
+  userProperty: 'payload'
+});
+
 var ctrlbooks = require('../controllers/books');
 var ctrlReviews = require('../controllers/reviews');
 
@@ -10,10 +17,10 @@ router.get('/books/:bookid', ctrlbooks.booksReadOne);
 router.put('/books/:bookid', ctrlbooks.booksUpdateOne);
 router.delete('/books/:bookid', ctrlbooks.booksDeleteOne);
 // reviews
-router.post('/books/:bookid/reviews', ctrlReviews.reviewsCreate);
+router.post('/books/:bookid/reviews', auth, ctrlReviews.reviewsCreate);
 router.get('/books/:bookid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
-router.put('/books/:bookid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
-router.delete('/books/:bookid/reviews/:reviewid', ctrlReviews.reviewsDeleteOne);
+router.put('/books/:bookid/reviews/:reviewid', auth,  ctrlReviews.reviewsUpdateOne);
+router.delete('/books/:bookid/reviews/:reviewid', auth,ctrlReviews.reviewsDeleteOne);
 
 var ctrlAuth = require('../controllers/authentication');
 // authentication
