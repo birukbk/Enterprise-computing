91f23290078cf155e4d567a9a10771285444e4bd
Biruk G <birukbk@gmail.com>
2017-03-23 15:17:10 +0000

    Adds passport and config to application.

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 require('./app_api/models/db')
+require('./app_api/config/passport');
 
 var routes = require('./app_server/routes/index');
 var routesApi = require('./app_api/routes/index');
@@ -25,6 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(passport.initialize());
 app.use('/', routes);
 app.use('/api',routesApi);
 
