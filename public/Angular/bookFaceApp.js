5fc78a08ec394f72cdbaf7c8d3557a52d6c328c4
Biruk G <birukbk@gmail.com>
2017-03-25 19:37:55 +0000

    moves data delevery from express to angular

--- public/Angular/bookFaceApp.js
+++ public/Angular/bookFaceApp.js
@@ -1,1 +1,20 @@
 angular.module('bookFaceApp',[]);
+
+var bookListCtrl = function ($scope) {
+	$scope.data = {
+		books :[{
+      title: 'Programming in Scala',
+      bookAuthor: 'Martin Odersky',
+      rating: 3,
+      _id: '5370a35f2536f6785f8dfb6a'
+    },{
+      title: '1984',
+      bookAuthor: 'George orwel',
+      rating: 5,
+      _id: '5370a35f2536f6785f8dfb6a'
+    }]};
+};
+
+angular
+  .module('bookFaceApp')
+  .controller('bookListCtrl', bookListCtrl);
