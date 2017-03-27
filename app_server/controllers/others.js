82cef14c76ddbb5cd850852dc14ba5699287516f
Biruk G <birukbk@gmail.com>
2017-03-27 14:32:55 +0100

    Switch routhing from Express to Angular

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -2,3 +2,8 @@
 module.exports.about = function(req,res){
 res.render('generic-text',{title:'About'});
 };
+
+/* GET Angular SPA page */
+module.exports.angularApp = function(req, res){
+  res.render('layout', { title: 'bookFace' });
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,11 +4,10 @@ var ctrlbooks = require('../controllers/books');
 var ctrlOthers = require('../controllers/others');
 
 /* book pages */
-router.get('/', ctrlbooks.homelist);
-router.get('/book/:bookid', ctrlbooks.bookInfo);
-
-router.get('/book/:bookid/review/new', ctrlbooks.addReview);
-router.post('/book/:bookid/review/new', ctrlbooks.doAddReview);
+router.get('/', ctrlOthers.angularApp);
+// router.get('/book/:bookid', ctrlbooks.bookInfo);
+// router.get('/book/:bookid/review/new', ctrlbooks.addReview);
+// router.post('/book/:bookid/review/new', ctrlbooks.doAddReview);
 
 /* Other pages */
 router.get('/about', ctrlOthers.about);
