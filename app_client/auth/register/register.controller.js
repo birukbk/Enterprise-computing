d44664b57ab998d4fe587731075693ddf899cb23
Biruk G <birukbk@gmail.com>
2017-03-28 22:06:49 +0100

    handles the registration form submission.

--- app_client/auth/register/register.controller.js
+++ app_client/auth/register/register.controller.js
@@ -15,6 +15,28 @@
             password: ""
         };
         vm.returnPage = $book.search().page || '/';
-        vm.onSubmit = function() {};
+        vm.onSubmit = function() {
+            vm.formError = "";
+            if (!vm.credentials.name || !vm.credentials.email || !vm.credentials.password) {
+                vm.formError = "All fields required, please try again";
+                return false;
+            } else {
+                vm.doRegister();
+            }
+        };
+        vm.doRegister = function() {
+            vm.formError = "";
+            authentication
+                .register(vm.credentials)
+                .error(function(err) {
+                    vm.formError = err;
+                })
+                .then(function() {
+                    $location.search('page', null);
+                    $location.path(vm.returnPage);
+                });
+        };
+
+
     }
 })();
