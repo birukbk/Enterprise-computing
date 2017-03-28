a69fc9a879fb35aa5f8bc71f8d7f67239df89c7c
Biruk G <birukbk@gmail.com>
2017-03-28 21:29:10 +0100

    add reference of authentication to appClientFiles array in app.js.

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ var appClientFiles = [
   'app_client/about/about.controller.js',
   'app_client/bookDetail/bookDetail.controller.js',
   'app_client/reviewModal/reviewModal.controller.js',
+  'app_client/common/services/authentication.service.js',
   'app_client/common/services/bookFaceData.service.js',
   'app_client/common/filters/addHtmlLineBreaks.filter.js',
   'app_client/common/directives/ratingStars/ratingStars.directive.js',
