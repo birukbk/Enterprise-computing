a90e10d870e671cb66b8074bd39dc0fea667ce35
Biruk G <birukbk@gmail.com>
2017-03-28 16:01:30 +0100

    Adds onSubmit form handlers

--- app_client/reviewModal/reviewModal.controller.js
+++ app_client/reviewModal/reviewModal.controller.js
@@ -2,11 +2,18 @@
     angular
         .module('bookFaceApp')
         .controller('reviewModalCtrl', reviewModalCtrl);
-   
-    reviewModalCtrl.$inject = ['$modalInstance','bookData'];
-    function reviewModalCtrl($modalInstance,bookData) {
+
+    reviewModalCtrl.$inject = ['$modalInstance', 'bookData'];
+
+    function reviewModalCtrl($modalInstance, bookData) {
         var vm = this;
         vm.bookData = bookData;
+        
+        vm.onSubmit = function() {
+            console.log(vm.formData);
+            return false;
+        };
+        
         vm.modal = {
             cancel: function() {
                 $modalInstance.dismiss('cancel');
