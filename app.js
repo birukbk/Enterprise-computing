4b7c6e665f9d882d9f1f723647092d850626a762
Biruk G <birukbk@gmail.com>
2017-03-29 20:15:26 +0100

    adds icon

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ fs.writeFile('public/angular/bookFace.min.js', uglified.code, function (err){
 });
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
